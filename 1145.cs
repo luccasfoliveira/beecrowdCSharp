9d353ac095f11838e52accb475e54aa043d1018a
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-04-04 16:55:48 -0300

    Add files via upload

---
+++ 1145.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args) {
+        int x, y;
+        string[] values = Console.ReadLine().Split(' ');
+        x = int.Parse(values[0]);
+        y = int.Parse(values[1]);
+
+        for (int i = 1; i <= y; i++) {
+            Console.Write(i);
+            if (i % x == 0 || i == y) {
+                Console.WriteLine();
+            }
+            else {
+                Console.Write(" ");
+            }
+        }
+    }
+}

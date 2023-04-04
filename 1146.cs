952abfe6ac6953ddd354aa8ff12c58b614ba40fd
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-04-04 16:59:04 -0300

    Add files via upload

---
+++ 1146.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        while (true) {
+            int N = int.Parse(Console.ReadLine());
+            if(N == 0) {
+                break;
+            }
+            for(int i = 1; i <= N; i++) {
+                if(i == N) {
+                    Console.WriteLine($"{i}");
+                }
+                else {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}

d5fad3787346f17e0e2f06000abe00ad7781f3f4
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-04-04 16:56:14 -0300

    Add files via upload

---
+++ 1144.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++) {
+            Console.WriteLine($"{i} {Potencia(i, 2)} {Potencia(i, 3)}");
+            Console.WriteLine($"{i} {Potencia(i, 2)+1} {Potencia(i, 3)+1}");
+        }
+    }
+
+    static int Potencia(int n, int exp) {
+        int x = n;
+        for (int i = 1; i < exp; i++) {
+            n *= x;
+        }
+        return n;
+    }
+}

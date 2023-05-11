f6c04e77a0794353f1ffa84d94d0cd102f33d025
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-05-11 11:57:23 -0300

    Update 2234.cs

--- 2234.cs
+++ 2234.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         string[] txt = Console.ReadLine().Split();
-        Console.WriteLine($"{double.Parse(txt[0])/double.Parse(txt[1]):N2}");
+        Console.WriteLine($"{(double.Parse(txt[0])/double.Parse(txt[1])).ToString("F2", CultureInfo.InvariantCulture)}");
     }
 }

511ecc4b5fd2ca459027b5b3ff109ef1df4d7836
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-05-11 11:34:59 -0300

    Create 1149.cs

---
+++ 1149.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] txt = Console.ReadLine().Split();
+        var A = int.Parse(txt[0]);
+        var i = 1;
+        while (int.Parse(txt[i]) <= 0)
+        {
+            i++;
+        }
+        var N = int.Parse(txt[i]);
+        var soma = A;
+        for (i = 1; i < N; i++)
+        {
+            soma += A + i;
+        }
+        Console.WriteLine(soma);
+    }
+}

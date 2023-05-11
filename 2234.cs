ab3f581dfba314e30325e6a86f2acdad8be1aa1c
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2023-05-11 11:45:18 -0300

    Create 2234.cs

---
+++ 2234.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] txt = Console.ReadLine().Split();
+        Console.WriteLine($"{double.Parse(txt[0])/double.Parse(txt[1]):N2}");
+    }
+}
